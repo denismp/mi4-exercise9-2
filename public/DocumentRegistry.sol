fcc4dd89534705fdaf96049000a04ff1cae13421
Denis Putnam <denisputnam@verizon.net>
2020-03-27 15:10:00 -0400

    Added contract.

--- public/DocumentRegistry.sol
+++ public/DocumentRegistry.sol
@@ -11,21 +11,31 @@ contract DocumentRegistry {
 
     modifier onlyOwner() {
         // Todo: Implementation
+        require(contractOwner == msg.sender, 'Not a contract owner');
+        _;
     }
 
     constructor() public {
         // Todo: Implementation
+        contractOwner = msg.sender;
     }
 
     function add(string memory hash) public onlyOwner returns (uint256 dateAdded) {
         // Todo: Implementation
+        uint currentTime = block.timestamp;
+        Document memory doc = Document(hash, currentTime);
+        documents.push(doc);
+        return currentTime;
     }
 
     function getDocumentsCount() public view returns (uint256) {
         // Todo: Implementation
+        length = documents.length;
     }
 
     function getDocument(uint256 index) public view returns (string memory, uint256) {
         // Todo: Implementation
+        Document memory document = documents[index];
+        return (document.hash, document.dateAdded);
     }
 }
